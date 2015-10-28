c9a9d5a64eb6c3dce42a94408cae600c62d297b8
Tony Fruzza <tony@lightspeedsystems.com>
2015-10-28 12:24:16 -0700

    changing animation for shake, starting at new memory address to give room for a buffer screen. Removing BASIC startup because of exmoizer will take care of that jump

--- base.s
+++ base.s
@@ -1,3 +1,16 @@
+; - - Master Memory Layout - -
+; $0400 - $07e8 screen 1 memory, but we clear $0400 - $0800 for ease
+; $0800 - $0C00 Screen 2 memory
+; $0C01 - $2bcb All code and data so far. 2 x 1,000bytes used for screens + 5,000 for audio
+; $3800 - $3FFF Character RAM
+; $8000 - $8f36 SID
+; $9000 - $CFFF 16K of ram here.
+; $D000 - $DFFF VIC/SID/CIA/IO registers
+; $D800 - $DBFF Reserved Color ram
+
+; $A000 - $BFFF BASIC ROM
+; $E000 - $FFFF KERNEL ROM
+
 SCREENMEM       .equ $0400 ; Start of character screen map, color map is + $D400
 COLORMEM        .equ $D800
 VMEM            .equ $D000
@@ -26,22 +39,23 @@ DIGI_AUX_SPEED  .equ 123
 zpPtr1          .equ $ba
 zpPtr2          .equ $bc
 __SID__         .equ $D400
+INITIAL_WAIT    .equ 200
+AFTER_WAIT      .equ 255
 
-.org $0801
-;Tells BASIC to run SYS 2064 to start our program
-.byte $0C,$08,$0A,$00,$9E,' ','2','0','6','4',$00,$00,$00,$00,$00
+.org $0C01
 
-
+veryStart
     jsr intDigiSound
     jsr initRefreshCounter
 
     lda #COLOR_BLUE
     sta SCREEN_BORDER
+    lda #COLOR_BLACK
     sta SCREEN_BG_COLOR
 
     jsr clearScreen
-
-
+    lda #0
+    sta scrollX
     ; switch to 38 columns
     lda $d016
     and #%11110111
@@ -50,33 +64,23 @@ __SID__         .equ $D400
     lda $d011
     and #%11110111
     sta $d011
-;    jsr loadInScreenColorSet
-    jsr clearScreen
+
     ; Init where the image is located
     lda #<fsbat
     sta picPtr
     lda #>fsbat
     sta picPtr+1
 
+    ldx #0
+waitWithBlankScreen
     jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-
+    inx
+    cpx #INITIAL_WAIT
+    bne waitWithBlankScreen
 
 bringInTheBat
     jsr scrollScreenUp
     inc scrollX
-    jsr waitFrame
-    jsr waitFrame
     jsr waitFrame
     jsr waitFrame
 
@@ -90,32 +94,10 @@ scrollX     .byte 0
 playDigi    .byte 0
 
 mainLoop
-;    jsr clearScreen
-
-    lda #COLOR_BLUE
-    sta SCREEN_BORDER
-    sta SCREEN_BG_COLOR
     jsr waitFrame
     jsr waitFrame
     jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-;    jsr changeFullCharScreenBatFaceToOpen
-
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-;    jsr changeFullCharScreenBatFaceToClosed
-
-
+    jsr changeFullCharScreenBatFaceToHalfOpen
     jsr waitFrame
     jsr waitFrame
     jsr waitFrame
@@ -137,131 +119,158 @@ mainLoop
     jsr waitFrame
     jsr waitFrame
     jsr waitFrame
-    lda $d011
-    and #%11111000
-    ora #%00000001
-    sta $d011
-
-    jsr waitFrame
-    jsr waitFrame
-    lda $d011
-    and #%11111000
-    ora #%00000010
-    sta $d011
-
+    jsr changeFullCharScreenBatFaceToClosed
     jsr waitFrame
     jsr waitFrame
     jsr waitFrame
     jsr waitFrame
-    lda $d011
-    and #%11111000
-    ora #%00000011
-    sta $d011
-
+    jsr waitFrame
+    jsr changeFullCharScreenBatFaceToHalfOpen
+    jsr waitFrame
     jsr waitFrame
     jsr waitFrame
 
     lda $d011
     and #%11111000
-    ora #%00000100
-    sta $d011
-    jsr waitFrame
-    jsr waitFrame
-
-    lda $d011
-    and #%11111000
-    ora #%00000101
+    ora #%00000001 ; 1
     sta $d011
 
+    jsr waitFrame
+    jsr waitFrame
+    inc $d011 ; 2
+    jsr changeFullCharScreenBatFaceToClosed
+    jsr waitFrame
+    jsr waitFrame
+    jsr waitFrame
+    jsr waitFrame
+    inc $d011 ; 3
+    jsr waitFrame
+    jsr waitFrame
+    inc $d011 ; 4
+    jsr waitFrame
+    jsr waitFrame
+    inc $d011 ; 5
+    jsr waitFrame
+    jsr waitFrame
+    jsr changeFullCharScreenBatFaceToOpen
+    inc $d011 ; 6
+    jsr waitFrame
+    inc $d011 ; 7
+    jsr waitFrame
+    dec $d011 ; 6
+    jsr waitFrame
+    dec $d011 ; 5
+    jsr waitFrame
+    dec $d011 ; 4
+    jsr waitFrame
+    dec $d011 ; 3
+    jsr waitFrame
+    dec $d011 ; 2
+    jsr waitFrame
+    dec $d011 ; 1
+    jsr waitFrame
+    dec $d011 ; 0
+    jsr waitFrame
+    inc $d011 ; 1
+    jsr waitFrame
+    inc $d011 ; 2
+    jsr waitFrame
+    inc $d011 ; 3
+    jsr waitFrame
+    inc $d011 ; 4
+    jsr waitFrame
+    inc $d011 ; 5
+    jsr waitFrame
+    inc $d011 ; 6
+    jsr waitFrame
+    dec $d011 ; 5
+    jsr waitFrame
+    dec $d011 ; 4
+    jsr waitFrame
+    dec $d011 ; 3
+    jsr waitFrame
+    dec $d011 ; 2
+    jsr waitFrame
+    inc $d011 ; 3
+    jsr waitFrame
+    inc $d011 ; 4
+    jsr waitFrame
+    inc $d011 ; 5
+    jsr waitFrame
+    inc $d011 ; 6
+    jsr waitFrame
+    inc $d011 ; 7
+    jsr waitFrame
+    dec $d011 ; 6
+    jsr waitFrame
+    dec $d011 ; 5
+    jsr waitFrame
+    dec $d011 ; 4
+    jsr waitFrame
+    dec $d011 ; 3
+    jsr waitFrame
+    dec $d011 ; 2
+    jsr waitFrame
+    dec $d011 ; 1
+    jsr waitFrame
+    dec $d011 ; 0
+    jsr waitFrame
+    inc $d011 ; 1
+    jsr waitFrame
+    inc $d011 ; 2
+    jsr waitFrame
+    inc $d011 ; 3
+    jsr waitFrame
+    inc $d011 ; 4
+    jsr waitFrame
+    inc $d011 ; 5
+    jsr waitFrame
+    inc $d011 ; 6
+    jsr waitFrame
+    inc $d011 ; 7
+    jsr waitFrame
+    dec $d011 ; 6
+    jsr waitFrame
+    dec $d011 ; 5
+    jsr waitFrame
+    dec $d011 ; 4
+    jsr waitFrame
+    dec $d011 ; 3
+    jsr waitFrame
+    dec $d011 ; 2
+    jsr waitFrame
+    dec $d011 ; 1
+    jsr waitFrame
+    dec $d011 ; 0
+    jsr waitFrame
+    inc $d011 ; 1
+    jsr waitFrame
+    inc $d011 ; 2
+    jsr waitFrame
+    inc $d011 ; 3
+    jsr waitFrame
+    inc $d011 ; 4
+    jsr waitFrame
+    inc $d011 ; 5
+    jsr waitFrame
+    inc $d011 ; 6
+    jsr waitFrame
+    inc $d011 ; 7
+    jsr waitFrame
+    jsr changeFullCharScreenBatFaceToClosed
 
+    ldx #0
+waitAfterHere
     jsr waitFrame
-    jsr waitFrame
-;    jsr changeFullCharScreenBatFaceToOpen
-    lda $d011
-    and #%11111000
-    ora #%00000111
-    sta $d011
-
-;lda #COLOR_L_BLUE
-;sta SCREEN_BORDER
-;sta SCREEN_BG_COLOR
-
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-;    jsr changeFullCharScreenBatFaceToClosed
-;lda #COLOR_CYAN
-;sta SCREEN_BORDER
-;sta SCREEN_BG_COLOR
-
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-
-
-    lda $d011
-    and #%11111000
-    sta $d011
-
+    inx
+    cpx #AFTER_WAIT
+    bne waitAfterHere
+;    lda #COLOR_CYAN
+;    sta SCREEN_BORDER
 ;    jsr loadInZoomOut1
-
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-    jsr waitFrame
-
-;    jsr restartDigiSound
+    jmp veryStart
+justStayHere
+    jmp justStayHere
     jmp mainLoop
 
 clearScreen ; void ()
@@ -289,8 +298,14 @@ changeFullCharScreenBatFaceToOpen
     sta SCREENMEM + 699
     sta SCREENMEM + 700
     rts
+changeFullCharScreenBatFaceToHalfOpen
+    lda #98
+    sta SCREENMEM + 699
+    sta SCREENMEM + 700
+    rts
+
 changeFullCharScreenBatFaceToClosed
-    lda clearingChar
+    lda #BLOCK_CHAR
     sta SCREENMEM + 699
     sta SCREENMEM + 700
     rts
