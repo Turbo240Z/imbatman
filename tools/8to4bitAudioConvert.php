e05294184a6a87b7c1dacf3734e4e93950486cbe
Tony Fruzza <tony@lightspeedsystems.com>
2015-10-28 12:22:59 -0700

    packing 4bit into upper and lower nibbles.

--- tools/8to4bitAudioConvert.php
+++ tools/8to4bitAudioConvert.php
@@ -1,16 +1,29 @@
 <?php
     $options = getopt("f:t:");
-    echo $options['t']." .byte ";
+    echo $options['t']." .byte 16, ";
     $fh = fopen($options['f'], "rb");
     // Values are signed -128 - 128 convert to 0 - 16
+    $upperNibble = true;
+    $lastValue   = 0;
     for($i=0;$i<filesize($options['f']);$i++){
         $char = fread($fh, 1);
         $val  = unpack("c", $char);
         $val[1] = round(($val[1] + 128)/16);
-        echo $val[1];
-        if($i+1 != filesize($options['f'])){
-            echo ", ";
+        $val[1] = $val[1]==16?15:$val[1];
+
+        if($upperNibble){
+            $lastValue   = (int)$val[1] << 4;
+            $upperNibble = false;
+        }else{
+            //echo "Combining ".(int)$val[1]." and ".(int)$lastValue."\n";
+            $combined    = (int)$val[1] | (int)$lastValue;
+            $upperNibble = true;
+            echo $combined;
+            if($i+2 != filesize($options['f'])){
+                echo ", ";
+            }
         }
+
     }
     if(isset($options['z'])){
         echo ", 0";
